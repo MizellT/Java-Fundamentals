2a698fec3a57507c54a793b5920ef4cb7ab4fd8e
Tyler <MizellT@github.com>
2022-12-03 20:34:05 -0700

    completed exercises 4 and 5

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,35 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte b = 2;
+        System.out.println("byte b is: " + b);
+
+        boolean boo = false;
+        System.out.println("boolean boo is: " + boo);
+
+        char
+                letter = 'A';
+        System.out.println("Char letter is: " + letter);
+
+        short s = 100;
+        System.out.println("Short s is: " + s);
+
+        int in = 200;
+        System.out.println("Int in is: " + in);
+
+        long l = 1000;
+        System.out.println("Long l is: " + l);
+
+        float f = 100.56f;
+        System.out.println("Float f is: " + f);
+
+        double d = 1000.561;
+        System.out.println("Double d is: " + d);
+
+
+
+
+
 
     }
 
